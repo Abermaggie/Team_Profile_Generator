83c00f581ceb74757eb007700dfffc4262315438
Maggie Abernathy <mlabernathy05@gmail.com>
2022-12-19 22:20:10 -0600

    inquirer prompts added. Starting interactive prompts.

---
+++ index.js
@@ -0,0 +1,10 @@
+const { default: inquirer } = require('inquirer');
+const Inquirer = require('inquirer');
+
+function mgrPrompt () {
+    console.log("Please enter your team information");
+    inquirer 
+    .prompt ([
+        
+    ])
+}
