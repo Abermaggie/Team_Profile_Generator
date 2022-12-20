e615321f4036850325ba6ab884afbff6786c9160
Maggie Abernathy <mlabernathy05@gmail.com>
2022-12-20 15:24:59 -0600

    able to detect HTML.  some structure work done

--- Manager.js
+++ Manager.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const Mainmenu = require('./Mainmenu');
 const fs = require('fs');
+const pathConf = require('./Path');
 
 function mgrPrompt () {
     console.log("Please enter your manager information")
@@ -28,24 +29,66 @@ function mgrPrompt () {
         }
     ])
     .then((answers) => {
-        let empContent = generatePro(answers);
-        fs.writeFileSync('index.html', empContent, (err) => 
-        err? console.log(err) : console.log('Successfully created File!'));
-    })
-};
+        var empContent = generatePro(answers);
+        var empAppend = appendPro(answers);
+        if(pathConf() === true) {
+                fs.appendFileSync('index.html', empAppend, (err) => 
+                err? console.log(err) : console.log('Successfully created File!'));
+            } else {
+                fs.appendFileSync('index.html', empContent + empAppend, (err) => 
+                err? console.log(err) : console.log('Successfully created File!'))
+            }
+        });
+        };
 
-let generatePro = ({mgrName, mgrId, mgrEmail, mgrNumber}) =>
-`${mgrName}, ${mgrId}, ${mgrEmail}, ${mgrNumber}`;
-//     .then((answers) => {{
-//         let empContent = generatePro(answers);
-//         fs.readFile('TestFile.txt', function (err, data) {
-//                     if (err) throw err;
-//     })
+
+// appendEmp () => {
+//     fs.appendFileSync('index.html', empAppend, (err) => 
+//     err? console.log(err) : console.log('Successfully created File!'));
 // }
-// )};}
 
+// function generateEmp () {
+//     fs.appendFileSync('index.html', `${empContent} + ${appendPro}`, (err) => 
+//     err? console.log(err) : console.log('Successfully created File!'))
+// }
+//     // .then((answers) => {
+//     //     let empContent = generatePro(answers);
+//     //     fs.writeFileSync('index.html', empContent, (err) => 
+//     //     err? console.log(err) : console.log('Successfully created File!'));
+//     // })
+// // var appendInfo = generatePro(answers)
+let generatePro = ({mgrName}) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./dist/style.css">
+    <title>${mgrName}'s Team</title>
+</head>
+<body>
+        <header class="jumboTron">My Team</header>`;
 
-
-// mgrPrompt();
+let appendPro = ({mgrName, mgrId, mgrEmail, mgrNumber}) =>
+    `   <div class="teamContainer">
+            <div class="infoContainer">
+                <div class="infoBoxes">
+                    <div class="emTitle">
+                        <h1 class="name">${mgrName}
+                            <br>Manager</h1> 
+                            <ul class="stats">
+                                <li class="emId">ID: ${mgrId}</li>
+                                <li class="email">Email: ${mgrEmail}<br></li>
+                                <li class="office">Office Phone: ${mgrNumber}</li>
+                            </ul>
+                    </div>
+                </div>
+            </div>
+        </div>           
+        <script src=""></script>
+    </body>
+</html>`
+;
 
 module.exports = mgrPrompt;
--- index.js
+++ index.js
@@ -2,10 +2,32 @@ const inquirer = require('inquirer');
 const Mainmenu = require('./Mainmenu');
 const mgrPrompt = require('./Manager');
 const egrPrompt = require('./Engineer');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 // function to create HTML structure//
-const path ='./index.html'
+// const path ='./index.html'
+
+
+// var path ='./index.html'
+
+// function pathConf () {
+// fs.stat(path, (err, stats) => {
+//     if( !err ){
+//          if(stats.isFile()){
+//              console.log(stats.isFile());
+//          }
+   
+//          else if(stats.isDirectory()){
+//              console.log('is directory? ' + stats.isDirectory());
+//          }
+//      }
+//      else
+//          console.log(false); 
+// });
+// }
+// pathConf();
+
+
 
 // let indExist = () = fs.access(path);
 // // function indExist() {
